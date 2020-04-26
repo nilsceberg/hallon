3f78565d008ca7df144b4949fdd12d2da3019017
Nils Ceberg <nils.ceberg@gmail.com>
2020-04-26 17:21:33 +0200

    rasterizer: use rounding instead of flooring when drawing lines

--- src/rasterizer.rs
+++ src/rasterizer.rs
@@ -49,7 +49,7 @@ pub fn line(rt: &mut RenderTarget, shader: &dyn FragmentShader, a: &Vec2, b: &Ve
         let mut x = 0;
         while x != dx {
             x += dx.signum();
-            let y = ((x as f32) * slope).floor() as i32;
+            let y = ((x as f32) * slope).round() as i32;
             pixel(rt, shader, x + x1, y + y1);
         }
     } else {
@@ -57,7 +57,7 @@ pub fn line(rt: &mut RenderTarget, shader: &dyn FragmentShader, a: &Vec2, b: &Ve
         let mut y = 0;
         while y != dy {
             y += dy.signum();
-            let x = ((y as f32) * slope).floor() as i32;
+            let x = ((y as f32) * slope).round() as i32;
             pixel(rt, shader, x + x1, y + y1);
         }
     }
